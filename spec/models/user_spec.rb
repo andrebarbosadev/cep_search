86e662d9f880aa8f5d6a56fb4ec1fe95114429b3
Andre Barbosa <andrebarbosa.dev@gmail.com>
2020-08-01 02:53:30 -0300

    Added some tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:username) }
+    it { should validate_uniqueness_of(:username) }
+    it { should validate_presence_of(:email) }
+    it { should allow_value("email@addresse.foo").for(:email) }
+    it { should_not allow_value("email@addres@se.foo").for(:email) }
+    it { should validate_length_of(:password) }
+    it { should have_secure_password }
+  end
+
+  context 'Associations' do
+    it { should have_many(:search_history) }
+  end
 end
